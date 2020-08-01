61f6f428dfa1f033fa41b4306f25f2d5019248cb
ziya <ziya.yekta@gmail.com>
2020-08-01 00:10:47 -0700

    3.8 answers added

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -8,7 +8,8 @@ const app = express()
 app.set('port', process.env.PORT || 3000);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(morgan('combined'));
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
 
 
 
