ba29be8ca9c4f25495aad8b3fa3bf80aef048cc8
ziya <ziya.yekta@gmail.com>
2020-08-12 02:53:14 -0700

    4.8 answers added

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -1,21 +1,29 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
+blogsRouter.get('/', async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({})
+    if (blogs) {
       response.json(blogs)
-    })
+    } else {
+      response.status(400).end()
+    }
+  } catch(ex) {
+    next(ex)
+  }
+  
 })
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+blogsRouter.post('/', async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body)
+    const result = await blog.save()
+    response.status(201).json(result)
+  } catch(ex) {
+      next(ex)
+  }
+
 })
 
 module.exports = blogsRouter
---
+++ part4/blogList/tests/blogs.api.test.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+
+const initialBlogs = [
+
+{
+	title: 'Java',
+	author: 'Sina',
+	url: 'some url',
+	likes: 4
+},
+{
+	title: 'C',
+	author: 'Reza',
+	url: 'some other urls',
+	likes: 2
+}
+
+]
+
+beforeEach(async () => {
+	try {
+		console.log('inserting new data to db.')
+		await Blog.deleteMany({})
+		const blogs = await Blog.create(initialBlogs)
+		console.log(`blogs created: ${blogs}`)
+	} catch(ex) {
+		console.log(ex);
+	}
+
+})
+
+
+
+test('all notes returned as json', async () => {
+	try {
+		await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+	} catch(ex) {
+		console.log(ex);
+	}
+
+})
+
+test(`There are ${initialBlogs.length} inside db`, async () => {
+	try {
+		const response = await api.get('/api/blogs')
+		expect(response.body).toHaveLength(initialBlogs.length)
+	} catch(ex) {
+		console.log(ex);
+	}
+
+})
+
+afterAll(() => {
+	mongoose.connection.close()
+})
